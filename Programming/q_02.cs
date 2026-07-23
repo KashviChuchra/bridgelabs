d7773ed6a61c4dae40b37367c0b58e8f594eae4e
KashviChuchra20 <kashvi0793.be23@chitkara.edu.in>
2026-07-23 12:12:09 +0530

    sample ques2 added

---
+++ Programming/q_02.cs
@@ -0,0 +1,12 @@
+﻿string name = "Eric";
+string fromCity = "Chennai";
+string toCity = "Banglore";
+string viaCity = "Vellore";
+
+double distanceFromCtoV = 156.6;
+double timeFromCtoV = 4 * 60 + 4;
+double distanceFromVtoB = 211.8;
+double timeFromVtoB = 4 * 60 + 25;
+
+
+Console.WriteLine($"Displaying\nTotal Distance:\t{distanceFromCtoV + distanceFromVtoB}\nTotal Time:\t{timeFromCtoV + timeFromVtoB}");
