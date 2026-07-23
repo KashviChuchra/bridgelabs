98d0337e48c703732281f6faa6b5f6995dbfd478
KashviChuchra20 <kashvi0793.be23@chitkara.edu.in>
2026-07-23 12:13:06 +0530

    Level 1 pdf all ques added

---
+++ Programming/level1_ques/Program.cs
@@ -0,0 +1,78 @@
+﻿//string name = "Harry";
+//int birth_year = 2000;
+//int current_year = 2024;
+
+//Console.WriteLine($"Harry's age in 2024 is:{current_year - birth_year}");
+
+//string name = "Sam";
+//int maths = 94;
+//int physics = 95;
+//int chemistry = 100;
+//double avgMarks = (maths + physics + chemistry) / 3.0;
+//Console.WriteLine($"Displaying Result:\n Name:{name}\tMaths:{maths}\tPhysics:{physics}\tChemistry:{chemistry}\tAverage Marks:{avgMarks}");
+
+//double distance = 10.8;
+//double miles = distance * 1.6;
+//Console.WriteLine($"The distance of {distance} km in miles:\t{miles}");
+
+
+//Console.WriteLine("Enter Cost Price: ");
+//int cp = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Enter Selling Price: ");
+//int sp = Convert.ToInt32(Console.ReadLine());
+//int profit = sp - cp;
+//int profit_percentage = (profit * 100) / cp;
+//Console.WriteLine($"This Cost Price is INR {cp}");
+//Console.WriteLine($"This Selling Price is INR {sp}");
+//Console.WriteLine($"Profit Percentage: {profit_percentage}%");
+
+
+
+//int studentsCount = 3;
+//int pensCount = 14;
+//Console.WriteLine($"The pen per student is {pensCount/studentsCount} and remaining pens are {pensCount%studentsCount}");
+
+
+//int fee = 125000;
+//int discount = 10;
+//Console.WriteLine($"The discounted amount is {fee*discount/100} INR and the final discountes fee is {fee - (discount * fee / 100)}");
+
+//int radius = 6378;
+//double volume = 4.0 / 3.0 * 3.14 * radius * radius * radius;
+//Console.WriteLine($"The volume of the sphere with {radius} km is {volume}km^3\nThe volume of the sphere with {radius} mile is {volume*0.6213* 0.6213 * 0.6213}miles^3");
+
+//Console.WriteLine("Enter the km: ");
+//double km = double.Parse(Console.ReadLine());
+//Console.WriteLine($"The total mile is{km * 1.6} mile for the given {km} km");
+
+
+//Console.WriteLine("Enter Student Fee: ");
+//int fee = int.Parse(Console.ReadLine());
+//Console.WriteLine("Enter Student Discount: ");
+//int discount = int.Parse(Console.ReadLine());
+//Console.WriteLine($"The discounted amount is {fee * discount / 100} INR and the final discountes fee is {fee - (discount * fee / 100)}");
+
+
+
+//Console.WriteLine("Enter height: ");
+//int height = int.Parse(Console.ReadLine());
+//double heightInInches = height / 2.54;
+//double heightInFeet = heightInInches / 12;
+//Console.WriteLine($"Your height in cm is {height} while in feet is {heightInFeet} and in inches is {heightInInches}");
+
+
+
+
+//int b=Convert.ToInt32(Console.ReadLine());
+//int height = Convert.ToInt32(Console.ReadLine());
+//double area = 0.5 * b * height;
+//Console.WriteLine($"Your height in {height} while in feet is {height / 30.48} and inches is {height / 2.54}");
+
+
+//double unitPrice = float.Parse(Console.ReadLine());
+//int quantity = int.Parse(Console.ReadLine());
+//Console.WriteLine($"The total Purcahse is INR {unitPrice * quantity} if the quantity is {quantity} and unit price is INR {unitPrice}");
+
+
+//int n = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine($"The max no of handshake of {n} people is {(n * (n - 1)) / 2}");
