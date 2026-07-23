cc9ace01b1d21770eb962b25e49de9ed82bce3d2
KashviChuchra20 <kashvi0793.be23@chitkara.edu.in>
2026-07-23 11:00:05 +0530

    Programming/q_01.cs added

---
+++ Programming/q_01.cs
@@ -0,0 +1,5 @@
+﻿string name = "Sam";
+int rollNumber = 1;
+double percentMarks = 99.9;
+char result = 'P';
+Console.WriteLine($"Displaying Result:\n Name:{name}\tRoll Number:{rollNumber}\tPercent Marks:{percentMarks}\tResult:{result}");
