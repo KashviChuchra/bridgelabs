5c0b263520a10727a5cda30052b21a3da0931a02
KashviChuchra20 <kashvi0793.be23@chitkara.edu.in>
2026-07-23 23:22:58 +0530

    Remove unwanted files

--- Programming/q_01.cs
+++
@@ -1,5 +0,0 @@
-﻿string name = "Sam";
-int rollNumber = 1;
-double percentMarks = 99.9;
-char result = 'P';
-Console.WriteLine($"Displaying Result:\n Name:{name}\tRoll Number:{rollNumber}\tPercent Marks:{percentMarks}\tResult:{result}");
