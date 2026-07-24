2a936373b040cf483f013bfe531327b30539ede0
Kashvi Chuchra <148766998+KashviChuchra@users.noreply.github.com>
2026-07-24 21:20:08 +0530

    Add BasicProblems.cs

---
+++ Programming/BasicProblems.cs
@@ -0,0 +1,60 @@
+//Console.WriteLine("Welcome to BridgeLabz!");
+
+// int a=Convert.ToInt32(Console.ReadLine());
+// int b=Convert.ToInt32(Console.ReadLine());
+// int sum=a+b;
+// Console.WriteLine(sum);
+
+// Console.Write("Enter temp in Celsius: ");
+// double celsius=Convert.ToDouble(Console.ReadLine());
+// double fahrenheit=(celsius* 9 / 5) + 32;
+// Console.WriteLine("Temperature in Fahrenheit = " + fahrenheit);
+
+// Console.Write("Enter radius: ");
+// double radius=Convert.ToDouble(Console.ReadLine());
+// double area=3.14*radius*radius;
+// Console.WriteLine("Area of Circle = " + area);
+
+// Console.Write("Enter radius: ");
+// double radius=Convert.ToDouble(Console.ReadLine());
+// Console.Write("Enter height: ");
+// double height=Convert.ToDouble(Console.ReadLine());
+// double volume=3.14 * radius * radius * height;
+// Console.WriteLine("Volume of Cylinder = " + volume);
+
+// Console.Write("Enter Principal: ");
+// double principal=Convert.ToDouble(Console.ReadLine());
+// Console.Write("Enter Rate: ");
+// double rate=Convert.ToDouble(Console.ReadLine());
+// Console.Write("Enter Time: ");
+// double time=Convert.ToDouble(Console.ReadLine());
+// double simpleInterest=(principal * rate * time) / 100;
+// Console.WriteLine("Simple Interest = " + simpleInterest);
+
+// Console.Write("Enter length: ");
+// double length=Convert.ToDouble(Console.ReadLine());
+// Console.Write("Enter width: ");
+// double width=Convert.ToDouble(Console.ReadLine());
+// double perimeter=2*(length + width);
+// Console.WriteLine("Perimeter of Rectangle = " + perimeter);
+
+// Console.Write("Enter base: ");
+// double baseNumber =Convert.ToDouble(Console.ReadLine());
+// Console.Write("Enter exponent: ");
+// double exponent =Convert.ToDouble(Console.ReadLine());
+// double result =Math.Pow(baseNumber, exponent);
+// Console.WriteLine("Result = " + result);
+
+// Console.Write("Enter first number: ");
+// double num1=Convert.ToDouble(Console.ReadLine());
+// Console.Write("Enter second number: ");
+// double num2=Convert.ToDouble(Console.ReadLine());
+// Console.Write("Enter third number: ");
+// double num3=Convert.ToDouble(Console.ReadLine());
+// double average=(num1 + num2 + num3) / 3;
+// Console.WriteLine("Average = " + average);
+
+// Console.Write("Enter distance in km: ");
+// double kilometers=Convert.ToDouble(Console.ReadLine());
+// double miles=kilometers *0.621371;
+// Console.WriteLine("Distance in Miles = " + miles);
